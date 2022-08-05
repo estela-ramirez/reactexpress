4891c2102e739b833aacd9d9e26febdfbfb164b5
Estela Ramirez <ramirezestela01@gmail.com>
2022-08-05 11:56:47 -0700

    modified dummy json data

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -4,119 +4,52 @@ const router = express.Router();
 router.get('/table', (req, res) => {
     const str = [
         {
-          "userId": 1,
-          "id": 1,
-          "title": "quidem molestiae enim"
+          "ID": 1,
+          "State": "CA",
+          "CountyName": "Santa Barbara County",
+          "2010": "1000",
+          "2011": "2000",
+          "2012": "3000", 
+          "2013": "4000",
+          "2014": "5000",
+          "2015": "6000",
+          "2016": "7000", 
+          "2017": "8000",
+          "2018": "9000",
+          "2019": "10000",
+          "2020": "11000"
         },
         {
-          "userId": 1,
-          "id": 2,
-          "title": "sunt qui excepturi placeat culpa"
+          "ID": 2,
+          "State": "OR",
+          "CountyName": "Santa Oregon County",
+          "2010": "1002",
+          "2011": "2002",
+          "2012": "3002", 
+          "2013": "4002",
+          "2014": "5002",
+          "2015": "6002",
+          "2016": "7002", 
+          "2017": "8002",
+          "2018": "9002",
+          "2019": "10002",
+          "2020": "11002"
         },
         {
-          "userId": 1,
-          "id": 3,
-          "title": "omnis laborum odio"
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "non esse culpa molestiae omnis sed optio"
-        },
-        {
-          "userId": 1,
-          "id": 5,
-          "title": "eaque aut omnis a"
-        },
-        {
-          "userId": 1,
-          "id": 6,
-          "title": "natus impedit quibusdam illo est"
-        },
-        {
-          "userId": 1,
-          "id": 7,
-          "title": "quibusdam autem aliquid et et quia"
-        },
-        {
-          "userId": 1,
-          "id": 8,
-          "title": "qui fuga est a eum"
-        },
-        {
-          "userId": 1,
-          "id": 9,
-          "title": "saepe unde necessitatibus rem"
-        },
-        {
-          "userId": 1,
-          "id": 10,
-          "title": "distinctio laborum qui"
-        },
-        {
-          "userId": 2,
-          "id": 11,
-          "title": "quam nostrum impedit mollitia quod et dolor"
-        },
-        {
-          "userId": 2,
-          "id": 12,
-          "title": "consequatur autem doloribus natus consectetur"
-        },
-        {
-          "userId": 2,
-          "id": 13,
-          "title": "ab rerum non rerum consequatur ut ea unde"
-        },
-        {
-          "userId": 2,
-          "id": 14,
-          "title": "ducimus molestias eos animi atque nihil"
-        },
-        {
-          "userId": 2,
-          "id": 15,
-          "title": "ut pariatur rerum ipsum natus repellendus praesentium"
-        },
-        {
-          "userId": 2,
-          "id": 16,
-          "title": "voluptatem aut maxime inventore autem magnam atque repellat"
-        },
-        {
-          "userId": 2,
-          "id": 17,
-          "title": "aut minima voluptatem ut velit"
-        },
-        {
-          "userId": 2,
-          "id": 18,
-          "title": "nesciunt quia et doloremque"
-        },
-        {
-          "userId": 2,
-          "id": 19,
-          "title": "velit pariatur quaerat similique libero omnis quia"
-        },
-        {
-          "userId": 2,
-          "id": 20,
-          "title": "voluptas rerum iure ut enim"
-        },
-        {
-          "userId": 3,
-          "id": 21,
-          "title": "repudiandae voluptatem optio est consequatur rem in temporibus et"
-        },
-        {
-          "userId": 3,
-          "id": 22,
-          "title": "et rem non provident vel ut"
-        },
-        {
-          "userId": 3,
-          "id": 23,
-          "title": "incidunt quisquam hic adipisci sequi"
+          "ID": 3,
+          "State": "TX",
+          "CountyName": "Santa Texas County",
+          "2010": "1003",
+          "2011": "2003",
+          "2012": "3003", 
+          "2013": "4003",
+          "2014": "5003",
+          "2015": "6003",
+          "2016": "7003", 
+          "2017": "8003",
+          "2018": "9003",
+          "2019": "10003",
+          "2020": "11003"
         },
       ];
     res.end(JSON.stringify(str));
--- frontend/src/components/BootTable.js
+++ frontend/src/components/BootTable.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
+import cellEditFactory from "react-bootstrap-table2-editor";
 
 function BootTable() {
   useEffect(() => {
@@ -14,36 +15,97 @@ function BootTable() {
     const data = await fetch("/table");
     const items = await data.json();
     setItems(items);
+    console.log(items);
   };
 
   const columns = [
     {
-      dataField: "userId",
-      text: "Artist ID",
-      sort: true
+      dataField: "ID",
+      text: "Unique ID",
+      sort: true,
     },
     {
-      dataField: "id",
-      text: "Album Number",
-      sort: true
+      dataField: "State",
+      text: "State Name",
+      sort: true,
+    //   validator: (newValue, row, column)=>{
+    //     if(isNaN(newValue) === true) {
+    //         return {
+    //             valid: false,
+    //             message: "Enter a number value"
+    //         }
+    //     }
+    //     return true
+    //   }
+    },  // TO DO: add validators
+        // TO DO: make scrollable to right 
+        // TO DO: make table a component, don't take up the entire page
+    { 
+      dataField: "CountyName",
+      text: "County Name",
     },
     {
-      dataField: "title",
-      text: "Album Title"
-    }
+        dataField: "2010",
+        text: "2010",
+    },
+    {
+        dataField: "2011",
+        text: "2011",
+    },
+    {
+        dataField: "2012",
+        text: "2012",
+    },
+    {
+        dataField: "2013",
+        text: "2013",
+    },
+    {
+        dataField: "2014",
+        text: "2014",
+    },
+    {
+        dataField: "2015",
+        text: "2015",
+    },
+    {
+        dataField: "2016",
+        text: "2016",
+    },
+    {
+        dataField: "2017",
+        text: "2017",
+    },
+    {
+        dataField: "2018",
+        text: "2018",
+    },
+    {
+        dataField: "2019",
+        text: "2019",
+    },
+    {
+        dataField: "2020",
+        text: "2020",
+    },
   ];
 
   return (
     <div className="App">
-      Rental Prices across the U.S.
+      {/* Rental Prices across the U.S. */}
+      {/* <DataTable data={data}></DataTable> */}
       <BootstrapTable
-        keyField="userId"
+        keyField="id"
         data={items}
         columns={columns}
         striped
         hover
         condensed
-        pagination={paginationFactory()}
+        pagination={paginationFactory( {sizePerPage: 10} )}  //TO DO: fix overflow error 
+        cellEdit={ cellEditFactory({ 
+            mode: 'click',
+            blurToSave: true,
+        }) }
       />
     </div>
   );
